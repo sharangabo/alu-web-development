ce57da20a70a3d1bf1f8862a8abeaa91eef961f3
sharangabo <e.sharangab@alustudent.com>
2024-06-02 01:22:48 +0200

    authentication

---
+++ Session_authentication/models/__init__.py
@@ -0,0 +1,16 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from os import getenv
+
+DB_URL = getenv('DB_URL')
+
+engine = create_engine(DB_URL, pool_pre_ping=True)
+Session = sessionmaker(bind=engine)
+
+Base = declarative_base()
+
+from models.user import User
+from models.user_session import UserSession
+
+Base.metadata.create_all(engine)
