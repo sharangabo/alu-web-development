d31471c5a3b7882ff6af0def8d25e7a03348f8f9
sharangabo <e.sharangab@alustudent.com>
2024-06-02 01:02:13 +0200

    authentication

--- Session_authentication/api/v1/auth/session_exp_auth.py
+++ Session_authentication/api/v1/auth/session_exp_auth.py
@@ -1,21 +1,28 @@
-#!/usr/bin/env python3
-
 """
 Module for Session Authentication with expiration
 """
-
 from api.v1.auth.session_auth import SessionAuth
 import os
 from datetime import datetime, timedelta
 
+
 class SessionExpAuth(SessionAuth):
+    """
+    Class for Session Authentication with expiration
+    """
     def __init__(self):
+        """
+        Initializes the SessionExpAuth instance
+        """
         try:
             self.session_duration = int(os.getenv('SESSION_DURATION', 0))
         except ValueError:
             self.session_duration = 0
 
     def create_session(self, user_id=None):
+        """
+        Creates a new session with an expiration date
+        """
         session_id = super().create_session(user_id)
         if session_id is None:
             return None
@@ -26,6 +33,9 @@ class SessionExpAuth(SessionAuth):
         return session_id
 
     def user_id_for_session_id(self, session_id=None):
+        """
+        Returns the user ID associated with the given session ID
+        """
         if session_id is None or session_id not in self.user_id_by_session_id:
             return None
 
