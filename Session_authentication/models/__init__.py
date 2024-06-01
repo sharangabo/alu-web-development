a731b87b7569b84ed7f67a8db98f78e4f04dbb04
sharangabo <e.sharangab@alustudent.com>
2024-06-02 01:26:19 +0200

    authentication

--- Session_authentication/models/__init__.py
+++ Session_authentication/models/__init__.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python3
+""" DocDocDocDocDocDoc
+"""
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
